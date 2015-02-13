6bc0191a379e6000f728c0d670799925fd86a777
OlgaShiryakina <olgashiryakina@gmail.com>
2015-02-13 21:43:04 +0200

    merge 2 arrays

--- задачиJS/Массив/js/main.js
+++ задачиJS/Массив/js/main.js
@@ -1,45 +1,32 @@
-function fillAnArray(){
-	var emptyArray = [];
-	emptyArray.length = 10;
-	console.log(emptyArray.length);
-	for(i=0; i<=9; i++) {
-	  emptyArray[i] = (i+1);
-	  var valueArray= emptyArray[i],
-	      parentBlock =  $('.emptyArray');
-	      
-	      
-	  parentBlock.append(function() {
-	  	return '<span></span>'
-	  })
-	  var son = parentBlock.find('span:last');
-	  son.text(valueArray);
-	}
-	
-	console.log(emptyArray);
-}	
-	
-function calculateAmount(){	
-	var arrayNumbers = [2,45,6,7,8,12,3],
-	    arrayLength = arrayNumbers.length,
-        sum = arrayNumbers[0],
-        parentBox =  $('.calculateAmount');
-        
-    for(i=1; i<=arrayLength-1; i++) {
-    	var sumTotal = sum + arrayNumbers[i];
-    	sum = sumTotal;
-    	
+//создать массив, в цикле заполнить его числами от 0 до 10
+function createArray(n){
+    var array = [];
+    for (var i = 0; i < n; i++) {
+        array.push(i+1); // если от нуля, то просто i
     }
     
-    parentBox.append(function() {
-	  	return '<span></span>'
-	  })
-	  
-	 var son = parentBox.find('span:last');
-	  son.text(sum);
+    console.log(array);
+    return array;
+}	
+
+
+//создать массив чисел (произвольный), в цикле посчитать сумму всех элементов массива
+function calculateAmount(){
+	
+	var arrayNumbers = [2,45,6,7,8,12,3],
+	    arrayLength = arrayNumbers.length,
+        sum = arrayNumbers[0];
+        
+    for(var i=1; i<arrayLength; i++) {
+    	sum = sum + arrayNumbers[i];
+    }
+    
 		  
 	console.log(sum);
 }
 
+
+//создать массив (произвольно), найти его наибольший элемент (var t = [2,45,6,7,8,12,3])
 function findLargestElement(){
 	var arrayRandom = [2,45,6,7,8,12,3],
 	    arrayLength = arrayRandom.length,
@@ -47,75 +34,58 @@ function findLargestElement(){
 	    parentBox =  $('.findLargestElement');
 	    
 	    
-	for(i=1; i<=arrayLength-1; i++) {
+	for(var i=1; i<arrayLength; i++) {
 		var a = arrayRandom[i];
+		
 		if (most<a) {
 			most = a;
-		} else
-		{
-			most = most;
 		}
+		
 		console.log(most);
 		
 	}
 	
-	
-	
-	parentBox.append(function() {
-	  	return '<span></span>'
-	  })
-	  
-	 var son = parentBox.find('span:last');
-	  son.text(most);
 }	    
 
-function combinedIntoOne(){
-	var arrayFirst = [24,12,47,31,1],
-	    arraySecond = [2,45,6,7,8,12,3],
-	    arrayGeneral = arrayFirst.concat(arraySecond);
-	    
-	    console.log(arrayGeneral);
-	    
-	    
-	    
-	var parentBox =  $('.combinedIntoOne');   
-	parentBox.append(function() {
-	  	return '<span></span>'
-	  })
-	  
-	 var son = parentBox.find('span:last');
-	  son.text('[' + arrayGeneral + ']');
-}
 
-function calculateSquareRoot(){
-	var arrayInitial = [2,45,6,7,8,12,3],
-	    arrayLength = arrayInitial.length;
+//создать два масива, объединить их в новый массив (третий), состоящий из элементов обоих массивов
+function combineArray(a, b){
+	var arrayGeneral = [];
+	    
+	for(var i=0; i<a.length; i++) {
+		arrayGeneral[i] = a[i];
+	};
 	
-	for(i=0; i<=arrayLength-1; i++) {
-		var a = arrayInitial[i],
-		    b = a * a;
-		
-		console.log(b);
-		
-		
-	}
-	  
+	for(var i=0; i<b.length; i++) {
+		arrayGeneral.push(b[i]);
+	};
+	
+	
+	console.log(arrayGeneral);
+	    
 }
 
 
-
-
-
-
-
+//создать массив чисел, вывести в консоль квадрат каждого числа
+function calculatePowers(array){
+    var squares = [],
+        tmp;
+    for (var i = 0; i < array.length; i++) {
+        tmp = array[i];
+        // никакой сатаны с длиной, аккуратно добавляем элементы в конец
+        squares.push(tmp * tmp);
+        // можно еще так squares.push(Math.pow(tmp, 2));
+		console.log(squares[i]);
+    }
+    return squares;
+}
 
 
     
-	    
 $(document).ready(function(){
-	fillAnArray();
+	createArray(10);
 	calculateAmount();
 	findLargestElement();
-	combinedIntoOne();
-	calculateSquareRoot();
+	combineArray([24,12,47,31,1], [2,45,6,7,8,12,3]);
+	calculatePowers([2,45,6,7,8,12,3]);
 })
